41115a0b85182fc2274355cab188c92394af6aa6
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2020-08-07 21:37:46 -0300

    [add] Basic graphs input on mouse click

---
+++ Assets/Scripts/GrafosController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasRenderer))]
+public class GrafosController : MonoBehaviour
+{
+    
+}
