64dafafa6ed001bb9077c1cd120c5f8f1266ece7
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2020-08-09 22:14:43 -0300

    [add] Canvas procedural circle renderer

--- Assets/Scripts/Grafos/Grafo.cs
+++ Assets/Scripts/Grafos/Grafo.cs
@@ -8,12 +8,15 @@ public class Grafo : MonoBehaviour
 {
     [SerializeField] public TextMeshProUGUI grafoID;
     [SerializeField] public Image grafoSprite;
-
     [SerializeField] public GameObject graphicalComponents;
+    
+    private const float TAU = Mathf.PI * 2;
 
-    // monobehaviour
+    //private Vector3 center;
+
     void Awake()
     {
+        //center = transform.position;
         // provavelmente vamo faze algo aqui
     }
     void OnEnable()
@@ -28,6 +31,32 @@ public class Grafo : MonoBehaviour
 
 
 
+
+
+
+    void OnDrawGizmosSelected()
+    {
+        DrawCircleOfSpheres(30);
+        
+        //Gizmos.DrawLine();
+    }
+
+    void DrawCircleOfSpheres(int numberOfSpheres)
+    {
+        float radius = 100;
+        float sphereSize = 3;
+        Vector3 grafoPosition = transform.position;
+
+        for (float angle = TAU / numberOfSpheres; angle <= TAU; angle += TAU / numberOfSpheres)
+        {
+            float sin = Mathf.Sin(angle);
+            float cos = Mathf.Cos(angle);
+
+            Vector3 eachSpherePosition = new Vector3(radius * sin, radius * cos);
+
+            Gizmos.DrawSphere(grafoPosition + eachSpherePosition, sphereSize);
+        }
+    }
 
 
 
