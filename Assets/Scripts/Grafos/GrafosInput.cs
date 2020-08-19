3e085e5e0dd2799031eaa84b623198ce7ac7cb39
Rodrigo Sonego <rodrigoasonego@gmail.com>
2020-08-19 17:05:05 -0300

    [add] separated line instantiating mode

--- Assets/Scripts/Grafos/GrafosInput.cs
+++ Assets/Scripts/Grafos/GrafosInput.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GrafosInput : MonoBehaviour
 {
     [SerializeField] private GameObject grafoPrefab;
     [SerializeField] private GrafosController grafosPanel;
+    [SerializeField] private GameObject linePrefab;
 
     public static bool canUserCreateGrafos { get; set; } = true;
+    public static bool canUserDrawLine { get; set; } = true;
 
     private static bool isUserLeftClicking = false;
     private static bool isUserRightClicking = false;
 
-
+    private Vector3 initialLinePoint;
     private Vector3 mousePosition;
     private Quaternion grafoRotation;
     private Transform grafoParent;
+    public static InputMode inputMode = InputMode.grafo;
+
 
     void Awake()
     {
@@ -30,8 +36,14 @@ public class GrafosInput : MonoBehaviour
         isUserRightClicking = Input.GetMouseButtonDown(1);
         mousePosition = Input.mousePosition;
 
-        if ( canUserCreateGrafos && isUserLeftClicking ) { Instantiate(grafoPrefab, mousePosition, grafoRotation, grafoParent); } 
-        
+        if ( canUserCreateGrafos && isUserLeftClicking && inputMode == InputMode.grafo) {
+                Instantiate(grafoPrefab, mousePosition, grafoRotation, grafoParent);           
+        }
+        else if (isUserLeftClicking && inputMode == InputMode.line)
+        {
+            if (CanDrawLine()) { DrawLine(); }         
+        }
+
         if ( isUserRightClicking ) { DeleteIfPossible(); }
 
     }
@@ -46,6 +58,29 @@ public class GrafosInput : MonoBehaviour
         }
     }
 
+    private bool CanDrawLine()
+    {
+        bool hasInitialPoint = false;
+        if (canUserDrawLine)
+        {
+            if (initialLinePoint != Vector3.zero)
+                hasInitialPoint = true;
+            else
+            {
+                hasInitialPoint = false;
+                initialLinePoint = mousePosition;
+            }
+        }
+        
+        return hasInitialPoint && canUserDrawLine;
+    }
+
+    private void DrawLine()
+    {
+        GameObject line = Instantiate(linePrefab, grafoParent);
+        line.GetComponent<UILineRenderer>()._SetPositions(initialLinePoint, mousePosition);
+        initialLinePoint = Vector3.zero;
+    }
 
 
 
