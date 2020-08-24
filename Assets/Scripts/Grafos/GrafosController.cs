356a4a46135fb42b0446f9b985c8cb2878a63c5a
Rodrigo Sonego <rodrigoasonego@gmail.com>
2020-08-24 16:26:59 -0300

    [add] added stack for removed characters

--- Assets/Scripts/Grafos/GrafosController.cs
+++ Assets/Scripts/Grafos/GrafosController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class GrafosController : MonoBehaviour
 {
     public static List<Grafo> grafos = new List<Grafo>();
+    private static Stack<char> removedChars = new Stack<char>();
 
     private static char character = 'A';
 
@@ -17,15 +18,26 @@ public class GrafosController : MonoBehaviour
         print("created grafo " + grafo.grafoID.text);
     }
 
-    public static void RemoveFromGrafos(Grafo grafo) { grafos.Remove(grafo); }
+    public static void RemoveFromGrafos(Grafo grafo) {
+        grafos.Remove(grafo);
+        char grafoChar = grafo.grafoID.text.ToCharArray()[0];
+        removedChars.Push(grafoChar); 
+    }
 
     private static void AssignLetter(Grafo grafo)
     {
-        string currentChar = character.ToString();
-        grafo.grafoID.text = currentChar;
-        grafo.name = "grafo " + currentChar;
+        if(removedChars.Count == 0)
+        {
+            string currentChar = character.ToString();
+            grafo.grafoID.text = currentChar;
+            grafo.name = "grafo " + currentChar;
 
-        character++;
+            character++;
+        } else {
+            string currentChar = removedChars.Pop().ToString();
+            grafo.grafoID.text = currentChar;
+        }
+        
     }
 
     
